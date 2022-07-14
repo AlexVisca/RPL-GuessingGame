4cb9ecdd8075f520813cd65a99b29852d5c34892
AlexVisca <alexvisca@gmail.com>
2022-07-14 14:18:36 -0700

    test build milestone

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
 use std::io;
 // Main runtime
 fn main() {
     println!("Guess the number!");
+
+    let secret_int = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_int}");
 
     println!("Please input your guess:");
 
